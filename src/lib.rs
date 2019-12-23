45164a385d444b7f79a90c373ab4e48e450129a4
Samuel Sleight <samuel.sleight@gmail.com>
2019-12-23 12:51:24 +0000

    Split Lobby into LobbySender and LobbyReceiver types

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
-use std::io::{Error, self};
-use std::sync::mpsc::{channel, Sender, Receiver};
-use std::sync::{Arc, Mutex};
-use std::net::{ToSocketAddrs, TcpListener};
-use std::thread;
-
-use vec_map::VecMap;
+use std::{
+    sync::{
+        mpsc::{channel, Sender, Receiver},
+        Arc, Mutex,
+    },
+    io::{Error, self},
+    net::{ToSocketAddrs, TcpListener},
+    marker::PhantomData,
+    thread,
+};
 
 use serde::ser::Serialize;
 use serde::de::DeserializeOwned;
 
 use serde_json::to_vec;
 
+use vec_map::VecMap;
 use crate::connection::Connection;
 
 mod connection;
@@ -18,15 +22,30 @@ mod event;
 
 pub use crate::event::{Event, EventKind};
 
-pub struct Lobby<Data> {
-    listener_tx: Sender<()>,
+struct LobbyCloser {
+    tx: Sender<()>
+}
+
+pub struct LobbySender {
+    connections: Arc<Mutex<VecMap<Connection>>>,
+
+    _closer: Arc<Mutex<LobbyCloser>>,
+}
+
+pub struct LobbyReceiver<Data> {
     event_rx: Receiver<Event<Data>>,
 
-    connections: Arc<Mutex<VecMap<Connection>>>
+    _closer: Arc<Mutex<LobbyCloser>>,
+}
+
+pub struct Lobby<Data>{
+    _phantom: PhantomData<Data>
 }
 
 impl<Data> Lobby<Data> where Data: 'static + Send + DeserializeOwned {
-    pub fn spawn<A: ToSocketAddrs>(addr: A) -> io::Result<Lobby<Data>> {
+    pub fn spawn<A: ToSocketAddrs>(addr: A) -> io::Result<(LobbySender, LobbyReceiver<Data>)>
+        where Data: 'static + Send + DeserializeOwned 
+    {
         let listener = TcpListener::bind(addr)?;
         listener.set_nonblocking(true)?;
 
@@ -34,10 +53,10 @@ impl<Data> Lobby<Data> where Data: 'static + Send + DeserializeOwned {
         let (tx, event_rx) = channel();
 
         let connections = Arc::new(Mutex::new(VecMap::new()));
-        let thread_conns = connections.clone();
+        let thread_connections = connections.clone();
 
         thread::spawn(move || {
-            let connections = thread_conns;
+            let connections = thread_connections;
             let mut id = 0;
 
             loop {
@@ -46,25 +65,36 @@ impl<Data> Lobby<Data> where Data: 'static + Send + DeserializeOwned {
                 }
 
                 if let Ok((stream, addr)) = listener.accept() {
-                    let tx = tx.clone();
-                    let conn = Connection::spawn(id, stream, addr, connections.clone(), tx);
+                    let conn = Connection::spawn(id, stream, addr, connections.clone(), tx.clone());
                     connections.lock().unwrap().insert(id, conn);
                     id += 1;
                 }
             }
         });
 
-        Ok(Lobby {
-            listener_tx,
-            event_rx,
-            connections
-        })
-    }
+        let _closer = Arc::new(Mutex::new(LobbyCloser { tx: listener_tx }));
 
+        let sender = LobbySender {
+            connections,
+            _closer: _closer.clone()
+        };
+
+        let receiver = LobbyReceiver {
+            event_rx,
+            _closer
+        };
+
+        Ok((sender, receiver))
+    }
+}
+
+impl<Data> LobbyReceiver<Data> where Data: 'static + Send + DeserializeOwned {
     pub fn events<'a>(&'a self) -> impl Iterator<Item=Event<Data>> + 'a {
         self.event_rx.try_iter()
     }
+}
 
+impl LobbySender {
     pub fn send_to_pred<D, P>(&self, pred: P, data: D) -> Result<(), Vec<(usize, Error)>>
     where 
         P: Fn(usize) -> bool,
@@ -101,8 +131,8 @@ impl<Data> Lobby<Data> where Data: 'static + Send + DeserializeOwned {
     }
 }
 
-impl<Data> Drop for Lobby<Data> {
+impl Drop for LobbyCloser {
     fn drop(&mut self) {
-        self.listener_tx.send(()).unwrap();
+        self.tx.send(()).unwrap();
     }
 }
